2b5140e13a9e0b24b88336d776b9dfc9c2237676
Alastaire Balin <balinalastaire26@gmail.com>
2025-12-04 20:06:46 -0500

    Added "View All Students" button (WIP) and started Advisor Blueprint
    Advisor_dashboard (Streamlit):
    - Setup ViewAllStudents but need to add students page
    
    Advisor_route (backend):
    - imported blueprint, json, and request alongside db
    - made the blueprint for advisor
    - added GET for returning all advisors from database (db)

---
+++ api/backend/advisor/advisor_route.py
@@ -0,0 +1,17 @@
+import logging
+from flask import Blueprint, request, jsonify
+from backend.db_connection.db import db
+
+logger = logging.getLogger(__name__)
+
+advisor_bp = Blueprint('advisor', __name__, url_prefix='/api/advisor')
+
+#GET /advisor
+@advisor_bp.get('/')
+def get_advisors():
+    conn = db.get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT advisorID, fName, lName, department, email FROM advisor")
+    advisors = cursor.fetchall()
+    return jsonify(advisors), 200
